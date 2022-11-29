7f0bc528010ac2139105f313a6d3187f1a534267
AlekhinAVe <aalekhin2004@yandex.ru>
2022-11-29 09:38:38 +0300

    Созданы массивы изображений для анимации

--- constants.py
+++ constants.py
@@ -18,3 +18,26 @@ class BlockType:
 block_images = {BlockType.grass: pg.image.load("textures/tile_grass.jpg"),
                 BlockType.dirt: pg.image.load("textures/tile_dirt.png"),
                 BlockType.sky: pg.image.load("textures/tile_sky.png"), }
+
+playerStand = pg.image.load("animation/character_male_idle.png")
+
+
+animRight = [pg.image.load("animation/character_male_right_walk0.png"),
+             pg.image.load("animation/character_male_right_walk1.png"),
+             pg.image.load("animation/character_male_right_walk2.png"),
+             pg.image.load("animation/character_male_right_walk3.png"),
+             pg.image.load("animation/character_male_right_walk4.png"),
+             pg.image.load("animation/character_male_right_walk5.png"),
+             pg.image.load("animation/character_male_right_walk6.png"),
+             pg.image.load("animation/character_male_right_walk7.png")]
+
+animLeft = [pg.image.load("animation/character_male_left_walk0.png"),
+            pg.image.load("animation/character_male_left_walk1.png"),
+            pg.image.load("animation/character_male_left_walk2.png"),
+            pg.image.load("animation/character_male_left_walk3.png"),
+            pg.image.load("animation/character_male_left_walk4.png"),
+            pg.image.load("animation/character_male_left_walk5.png"),
+            pg.image.load("animation/character_male_left_walk6.png"),
+            pg.image.load("animation/character_male_left_walk7.png")]
+
+playerJumped = pg.image.load("animation/character_maleAdventurer_jump.png")
